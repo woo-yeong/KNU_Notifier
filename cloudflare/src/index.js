ad3bd72090aa2b59a6f8179608c388f8222b1ca5
woo-yeong <tjdndud73@knu.ac.kr>
2026-08-24 18:41:28 +0900

    Stop monitoring field training notices

--- cloudflare/src/index.js
+++ cloudflare/src/index.js
@@ -25,14 +25,6 @@ const SOURCES = [
     color: 0xe67e22,
     kind: "knujob",
   },
-  {
-    key: "field_training",
-    name: "현장실습 공지사항",
-    url: "https://home.knu.ac.kr/HOME/knujob/sub.htm?nav_code=knu1623826179",
-    emoji: "🟣",
-    color: 0x8e44ad,
-    kind: "knujob",
-  },
 ];
 
 const USER_AGENT = "KNU-Notice-Discord-Notifier/2.0";
